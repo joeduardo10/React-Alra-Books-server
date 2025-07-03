d0b86d624017e35d6253c9bef674dac2923fca69
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2025-07-02 21:54:55 -0300

    rotas

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const rotaLivro = require('./rotas/livro')
 const app = express();
-const port = 8000
+const port = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Olá mundo Puta que o paril, me livrei da maldição')}
-  );
+app.use('/livros', rotaLivro);
+
 
 app.listen(port, () => {
   console.log(`Servidor escutando na porta  ${port}`)
---
+++ rotas/livro.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.send('Olá mundo Puta que o paril, me livrei da maldição')});
+
+
+module.exports = router;
