43ddc964e8b308d4d38c24ef73f9c57467336dc8
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2025-07-02 22:50:15 -0300

    http erros

--- rotas/livro.js
+++ rotas/livro.js
@@ -3,7 +3,13 @@ const { Router } = require("express");
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.send('Olá mundo Puta que o paril, me livrei da maldição')});
+    try{
+        res.send('Olá mundo Puta que o paril, me livrei da maldição')
+    }catch(error){
+        res.status(500)
+        res.send(error.message) 
+    }
+  });
 
 router.patch('/', (req, res) => {
     res.send('você fez uma requisição PATCH')});
